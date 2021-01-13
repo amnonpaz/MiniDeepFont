191348f15baf4078c8bf5d885564b8b4ebe64046
Amnon Paz <amnon@railvision.io>
2021-01-13 17:33:29 +0200

    Preprocess: Normailze only change to YUV

--- Preprocess.py
+++ Preprocess.py
@@ -8,9 +8,12 @@ from skimage import util, transform
 Normalize the mean and std of each channel in an image
 '''
 def normalize_image(image):
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2YUV).astype('float32')
-    mean, std = image.mean(axis=(0, 1)), image.std(axis=(0, 1))
-    return (image - mean) / std / 255
+    return cv2.cvtColor(image, cv2.COLOR_BGR2YUV).astype('float32')
+
+#    image = cv2.cvtColor(image, cv2.COLOR_BGR2YUV).astype('float32')
+#    return image / 255
+#    mean, std = image.mean(axis=(0, 1)), image.std(axis=(0, 1))
+#    return (image - mean) / std / 255
 
 
 '''
