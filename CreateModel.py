b6d1a2edadb4c360190f542f77848f14cc2e4969
Amnon Paz <amnon@railvision.io>
2021-01-17 22:33:06 +0200

    CreateModel: Allow several files for each DB

--- CreateModel.py
+++ CreateModel.py
@@ -55,33 +55,34 @@ def db_add_datadet(db_group, img, letter, filename: str, idx: str, label = None)
         dataset.attrs['letter'] = letter
 
 
-def prepare_database(hdf5_input: str, hdf5_output: str, shape: tuple,
+def prepare_database(input_files: list, hdf5_output: str, shape: tuple,
                      affine_crop=False, do_norm=True, rewrite=False, store_labels=True):
 
-    with h5py.File(hdf5_input, 'r') as images_db:
-        if not rewrite and path.exists(hdf5_output):
-            return
+    for hdf5_input in input_files:
+        with h5py.File(hdf5_input, 'r') as images_db:
+            if not rewrite and path.exists(hdf5_output):
+                return
 
-        with h5py.File(hdf5_output, 'w') as fonts_db:
-            images_group = fonts_db.create_group('images')
+            with h5py.File(hdf5_output, 'w') as fonts_db:
+                images_group = fonts_db.create_group('images')
 
-            print('Processing file {F}'.format(F=hdf5_input))
+                print('Processing file {F}'.format(F=hdf5_input))
 
-            for key in images_db['data'].keys():
-                img = images_db['data'][key][:]
-                if do_norm:
-                    img = Preprocess.normalize_image(img)
+                for key in images_db['data'].keys():
+                    img = images_db['data'][key][:]
+                    if do_norm:
+                        img = Preprocess.normalize_image(img)
 
-                bboxes = images_db['data'][key].attrs['charBB']
-                fonts = images_db['data'][key].attrs['font']
-                letters = ''.join([word.decode('utf-8') for word in images_db['data'][key].attrs['txt']])
+                    bboxes = images_db['data'][key].attrs['charBB']
+                    fonts = images_db['data'][key].attrs['font']
+                    letters = ''.join([word.decode('utf-8') for word in images_db['data'][key].attrs['txt']])
 
-                for bb_idx in range(bboxes.shape[-1]):
-                    bb = bboxes[:, :, bb_idx]
-                    character = Preprocess.crop_character(img, bb, shape, affine_crop)
-                    label = encode_font_name(fonts[bb_idx].decode('utf-8')) if store_labels else None
+                    for bb_idx in range(bboxes.shape[-1]):
+                        bb = bboxes[:, :, bb_idx]
+                        character = Preprocess.crop_character(img, bb, shape, affine_crop)
+                        label = encode_font_name(fonts[bb_idx].decode('utf-8')) if store_labels else None
 
-                    db_add_datadet(images_group, character, letters[bb_idx], key, bb_idx, label)
+                        db_add_datadet(images_group, character, letters[bb_idx], key, bb_idx, label)
 
 
 def load_database(filename: str, load_labels = True):
@@ -114,15 +115,15 @@ def store_results(dest_filename, predictions, filenames, letters):
 
 
 if __name__ == '__main__':
-    input_filename = 'datasets/train/SynthText.h5'
-    validation_filename = 'datasets/validation/SynthTextValidation.h5'
+    input_filenames = [ 'datasets/train/SynthText.h5', 'datasets/train/train.h5' ]
+    validation_filename = [ 'datasets/validation/SynthTextValidation.h5' ]
     font_train_db = 'datasets/train/ExtractedFonts.h5'
     font_validation_db = 'datasets/validation/ExtractedFontsValidation.h5'
 
     shape = (28, 28)
 
-    prepare_database(input_filename, font_train_db, shape, rewrite=False)
-    prepare_database(validation_filename, font_validation_db, shape, rewrite=False)
+    prepare_database(input_filenames, font_train_db, shape, rewrite=True)
+    prepare_database(validation_filename, font_validation_db, shape, rewrite=True)
 
     train_x, train_y, _, _ = load_database(font_train_db)
     validate_x, validate_y, validate_letters, validate_filenames = load_database(font_validation_db)
